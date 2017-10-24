6954a549c31ac86f5f73a77e61ad146e7d2de4bf
SunShineM <18301310746@163.com>
2017-10-24 10:17:28 +0800

    '10/24'

--- js/package.js
+++ js/package.js
@@ -102,3 +102,57 @@ function downLoadAppFun(){
       // location.href=''; 
   }
 }
+//检索时间是多久前
+function timeCheckFun(startTime,endTime){
+  var t = endTime-startTime;
+  //获取年数时间差
+  var year=parseInt((t/(1000*60*60*24))/365);
+  //获取月数时间差
+  var month=parseInt((t/(1000*60*60*24))/30);
+  //获取天数时间差
+  var days=parseInt(t/(1000*60*60*24));
+  //获取小时时间差
+  var HH=parseInt(t/(1000*60*60));
+  //获取分钟时间差
+  var mm=parseInt(t/(1000*60));
+  //获取秒时间差
+  var ss=parseInt(t/(1000));
+  var gaidongtime;
+  if(days<=1){
+    if(ss<=60){
+      gaidongtime="刚刚";
+    }else if(ss>60&&ss<=60*60){
+      gaidongtime=mm+"分钟前";
+    }else if(ss>60*60&&ss<=60*60*24){
+      gaidongtime=HH+"小时前";
+    }else{
+      gaidongtime=days+"天前";
+    }
+  }else if(days<30&&days>1){
+    gaidongtime=days+"天前";
+  }else if(days>=30&&days<365){
+    var days1=parseInt(days/30);
+    gaidongtime=days1+"月前";
+  }else if(days>=365){
+    var days2=parseInt(days/365);
+    gaidongtime=days2+"年前";
+  }else{
+    gaidongtime="历史很久远";
+  }
+  return gaidongtime;
+ }
+//  声音类型
+ function voiceType(x){
+  var obj = {
+    "1":"女高音",
+    "2":"次女高音",
+    "3":"女中音",
+    "4":"女低音",
+    "5":"男高音",
+    "6":"男中音",
+    "7":"低男中音",
+    "8":"男低音",
+    "9":"假声男高音"
+  }
+  return obj[x];
+}
